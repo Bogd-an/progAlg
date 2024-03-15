776e8d185c7dd91eaa8273174ed5369fea24f96a
Bogd-an <pogorielov.bogdan@gmail.com>
2024-03-15 08:08:13 +0200

    новий костиль

--- 3 list/list.cpp
+++ 3 list/list.cpp
@@ -114,35 +114,24 @@ public:
     }
 };
 
+#define TEST_LIST(name) \
+for(int i=5; i>0; i--) name.insertNode(i);\
+    dataNodeNext3 = name.searchNode(3)->next->data;\
+    cout << #name << " node 3 next node - " << dataNodeNext3  << std::endl;\
+    name.printList();\
+    name.deleteNode(1);\
+    name.deleteNode(5);\
+    name.deleteNode(10);\
+    name.printList();
+
 int main() {
 
     LinkedList linkedList;
     CircularLinkedList circularLinkedList;
     int dataNodeNext3;
 
-    // Додавання елементів у зв'язний список
-    for(int i=5; i>0; i--) linkedList.insertNode(i);
-
-    dataNodeNext3 = linkedList.searchNode(3)->next->data;
-    cout << "linkedList node 3 next node - " << dataNodeNext3  << endl;
-
-    linkedList.printList();
-    linkedList.deleteNode(1);
-    linkedList.deleteNode(5);
-    linkedList.deleteNode(10);
-    linkedList.printList();
-
-    // Додавання елементів у зв'язний список
-    for(int i=5; i>0; i--) circularLinkedList.insertNode(i);
-
-    dataNodeNext3 = circularLinkedList.searchNode(3)->next->data;
-    cout << "circularLinkedList node 3 next node - " << dataNodeNext3  << endl;
-
-    circularLinkedList.printList();
-    circularLinkedList.deleteNode(1);
-    circularLinkedList.deleteNode(5);
-    circularLinkedList.deleteNode(10);
-    circularLinkedList.printList();
+    TEST_LIST(linkedList);
+    TEST_LIST(circularLinkedList);
     return 0;
 }
 
@@ -153,4 +142,44 @@ linkedList node 3 next node - 4
 circularLinkedList node 3 next node - 4
 1 2 3 4 5
 2 3 4
+*/
+
+
+// TODO:
+/*
+Уникайте використання простору імен using namespace std: Використання 
+цього оператора може призвести до конфліктів імен, що може бути проблематичним 
+у великих проектах. Замість цього краще вказати std:: перед об'єктами стандартної 
+бібліотеки, як у вашому випадку.
+
+Використання const:
+
+У методах searchNode та printList ви можете використовувати ключове слово const, 
+щоб позначити, що ці методи не змінюють стан об'єкту класу.
+Параметр data в методі searchNode також може бути помічений як const, оскільки 
+він не має змінюватися.
+Перевірка на nullptr: У методі searchNode в кінці циклу ви використовуєте while 
+(temp->next), але потім перевіряєте if (!temp->next). Це може призвести до помилки 
+сегментації у випадку, якщо temp->next вказує на nullptr. Краще перевіряти на 
+nullptr сам вузол temp.
+
+Оптимізація методу вставки в CircularLinkedList: При вставці нового вузла у методі 
+insertNode в CircularLinkedList, ви копіюєте дані з першого елементу вузла, а потім 
+встановлюєте нові дані в початковий вузол. Це непотрібне копіювання. Простіше просто
+встановити нові дані в новий вузол, а потім перемістити посилання на початковий 
+вузол.
+
+Зменшення зайвої логіки у методі deleteNode в CircularLinkedList: У методі deleteNode
+ви використовуєте зайвий цикл while та перевірку на temp->next != head для того, 
+щоб знайти останній елемент у круговому списку перед видаленням. Це може бути 
+оптимізовано, оскільки останній елемент можна знайти за допомогою перевірки, чи 
+наступний елемент вказує на початковий вузол.
+
+Додавання коментарів до коду: Хоча код написаний досить зрозуміло, додавання 
+коментарів 
+може допомогти іншим розуміти вашу імплементацію.
+
+Перевірка на наявність пам'яті: Ваш код не перевіряє, чи була успішно виділена 
+пам'ять під нові вузли. Це може призвести до витоку пам'яті у випадку, якщо new не 
+може виділити пам'ять. Тому слід додати перевірку на nullptr після new.
 */
